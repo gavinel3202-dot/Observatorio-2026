7c5d1c9f59bee0d8a9347f12ff99d9b85df0e03b
gavinel3202-dot <gavinel3202@gmail.com>
2026-03-20 11:56:11 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
+jsimport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Observatorio-2026/',
   plugins: [react()],
 })
+```
